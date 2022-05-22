2fcea96283fcfa788cdc6da272c7e3aece764bb7
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-05-22 21:52:36 +0300

    Added patient login and register views

--- src/main/java/com/example/qualifiedwork/authenticaton/PatientLoginController.java
+++ src/main/java/com/example/qualifiedwork/authenticaton/PatientLoginController.java
@@ -1,9 +1,39 @@
 package com.example.qualifiedwork.authenticaton;
 
 import com.example.qualifiedwork.StartApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class PatientLoginController {
 
+    @FXML
+    private Button patientAuthBtn;
+
+    @FXML
+    private Button patientBackToChoiceBtn;
+
+    @FXML
+    private TextField patientLoginFiled;
+
+    @FXML
+    private PasswordField patientPasswordField;
+
+    @FXML
+    private Hyperlink patientRegisterBtn;
+
+    @FXML
+    void backToChoiceView(MouseEvent event) {
+        startApp.switchToChoiceScene();
+    }
+
+    @FXML
+    void openRegisterView(MouseEvent event) {
+        startApp.switchToPatientRegisterScene();
+    }
 
     private StartApp startApp;
 
--- src/main/java/com/example/qualifiedwork/authenticaton/PatientRegisterController.java
+++ src/main/java/com/example/qualifiedwork/authenticaton/PatientRegisterController.java
@@ -1,8 +1,36 @@
 package com.example.qualifiedwork.authenticaton;
 
 import com.example.qualifiedwork.StartApp;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 public class PatientRegisterController {
+
+    @FXML
+    private Button patientBackToLoginBtn;
+
+    @FXML
+    private TextField patientLoginFiled;
+
+    @FXML
+    private TextField patientLoginFiled1;
+
+    @FXML
+    private PasswordField patientPasswordField;
+
+    @FXML
+    private PasswordField patientPasswordField1;
+
+    @FXML
+    private Button patientRegisterBtn;
+
+    @FXML
+    void backToLogin(MouseEvent event) {
+        startApp.switchToPatientLoginScene();
+    }
 
     private StartApp startApp;
 
