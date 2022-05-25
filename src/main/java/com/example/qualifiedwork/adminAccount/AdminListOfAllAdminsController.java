3dc8c5556fb8b0f2940a1b88d8689c85af7fb8df
Tigashov Philipp <tigashov.filipp@yandex.ru>
2022-05-25 23:35:08 +0300

    Added opportunity to add data into table

--- src/main/java/com/example/qualifiedwork/adminAccount/AdminListOfAllAdminsController.java
+++ src/main/java/com/example/qualifiedwork/adminAccount/AdminListOfAllAdminsController.java
@@ -12,6 +12,7 @@ import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -76,7 +77,7 @@ public class AdminListOfAllAdminsController implements Initializable {
     private TextField passwordField;
 
     @FXML
-    private ChoiceBox<?> responsStatusChoice;
+    private ChoiceBox<String> responsStatusChoice;
 
     @FXML
     private TextField secondNameField;
@@ -97,6 +98,89 @@ public class AdminListOfAllAdminsController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        responsStatusChoice.setValue("Гл. врач");
+        responsStatusChoice.getItems().add("Гл. врач");
+        responsStatusChoice.getItems().add("Зав. отделения");
+        responsStatusChoice.getItems().add("Гл. медсестра");
+
+        showDataFromTable();
+    }
+
+    @FXML
+    void addNewRecordIntoTable(MouseEvent event) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        String secondName = secondNameField.getText().trim();
+        String name = nameField.getText().trim();
+        String fatherName = fatherNameField.getText().trim();
+        String birthDate = birthDateField.getText().trim();
+        String employDate = dateEmplField.getText().trim();
+        String responsStatus = responsStatusChoice.getValue();
+        String login = loginField.getText().trim();
+        String password = passwordField.getText().trim();
+
+        try {
+            connection = DBHandler.getConnection();
+            preparedStatement = connection.prepareStatement("INSERT INTO adminDefaultData (secondName, name, fatherName, birthDate, emplDate, responsStatus, login, password) VALUES" +
+                    " (?, ?, ?, ?, ?, ?, ?, ?) ");
+            preparedStatement.setString(1, secondName);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, fatherName);
+            preparedStatement.setString(4, birthDate);
+            preparedStatement.setString(5, employDate);
+            preparedStatement.setString(6, responsStatus);
+            preparedStatement.setString(7, login);
+            preparedStatement.setString(8, password);
+
+            preparedStatement.executeUpdate();
+            showDataFromTable();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+//        Connection connection = null;
+//        PreparedStatement psCheckUser = null;
+//        PreparedStatement psRegisterNewUser = null;
+//        PreparedStatement psRegisterNewUserData = null;
+//        ResultSet resultSet = null;
+//
+//        try {
+//            connection = DBHandler.getConnection();
+//            psCheckUser = connection.prepareStatement("SELECT * FROM patientsAuth WHERE login = ?");
+//            psCheckUser.setString(1, login);
+//
+//            resultSet = psCheckUser.executeQuery();
+//
+//            if (resultSet.isBeforeFirst()) {
+//                startApp.showErrorLoginAlert("Ошибка регистрации", "Пользователь с данным логином уже есть в системе.\nПопробуйте использовать другой.");
+//                connection.close();
+//            } else {
+//                psRegisterNewUser = connection.prepareStatement("INSERT INTO patientsAuth (login, password) VALUES (?, ?)");
+//                psRegisterNewUser.setString(1, login);
+//                psRegisterNewUser.setString(2, password);
+//
+//                psRegisterNewUser.executeUpdate();
+//
+//                psRegisterNewUserData = connection.prepareStatement("INSERT INTO patientDefaultData (secondName, name, fatherName, login) VALUES (?, ?, ?, ?)");
+//                psRegisterNewUserData.setString(1, secondName);
+//                psRegisterNewUserData.setString(2, name);
+//                psRegisterNewUserData.setString(3, fatherName);
+//                psRegisterNewUserData.setString(4, login);
+//
+//                psRegisterNewUserData.executeUpdate();
+//                connection.close();
+//
+//
+//                startApp.showSuccessMessage("Уведомление о регистрации", "Регистрация прошла успешно!", "Пользователь был добавлен в систему.\nТеперь вы можете выполнить авторизацию.");
+//                startApp.switchToPatientLoginScene();
+//            }
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private void showDataFromTable() {
+        listOfAdmins.getItems().clear();
         try {
             Connection connection = DBHandler.getConnection();
             ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM adminDefaultData");
